750526f3321928800ad4c161faa8e7d26b22f35b
Matt Venables <matt@blendist.com>
2011-02-21 11:15:07 -0500

    Revert cookie domain change

--- lib/authlogic/session/cookies.rb
+++ lib/authlogic/session/cookies.rb
@@ -116,12 +116,13 @@ module Authlogic
           def save_cookie
             controller.cookies[cookie_key] = {
               :value => "#{record.persistence_token}::#{record.send(record.class.primary_key)}",
-              :expires => remember_me_until
+              :expires => remember_me_until,
+              :domain => controller.cookie_domain
             }
           end
           
           def destroy_cookie
-            controller.cookies.delete cookie_key
+            controller.cookies.delete cookie_key, :domain => controller.cookie_domain
           end
       end
     end
